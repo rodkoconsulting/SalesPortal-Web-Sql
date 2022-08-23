0b26cf08e492b562f4caf05eb43d3e7e644336dc
arodko <31354392+arodko@users.noreply.github.com>
2022-08-23 10:57:05 -0400

    Initial commit TryConvertPaPrice function

---
+++ VersionSQL/POL-MAS-02/SAGE20/SalesPortal-Web/dbo/FunctionScalar/TryConvertPaPrice.sql
@@ -0,0 +1,10 @@
+﻿/****** Object:  Function [dbo].[TryConvertPaPrice]    Committed by VersionSQL https://www.versionsql.com ******/
+
+CREATE FUNCTION [dbo].[TryConvertPaPrice](@Value varchar(18))
+RETURNS decimal(15,6)
+AS
+BEGIN
+    SET @Value = RTRIM(LTRIM(REPLACE(@Value, ',', '')))
+    IF ISNUMERIC(@Value) = 0 RETURN 0.00
+    RETURN CONVERT(decimal(15,6), @Value)
+END
