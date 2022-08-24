d5ec01a74be91842b7aeedeaa077f334c975f7ac
arodko <31354392+arodko@users.noreply.github.com>
2022-08-24 18:20:56 -0400

    Initial commit PortalWebInvoiceMainProc Stored Procedure

---
+++ VersionSQL/POL-MAS-02/SAGE20/SalesPortal-Web/dbo/Procedure/PortalWebInvoiceMainProc.sql
@@ -0,0 +1,29 @@
+﻿/****** Object:  Procedure [dbo].[PortalWebInvoiceMainProc]    Committed by VersionSQL https://www.versionsql.com ******/
+
+CREATE PROCEDURE [dbo].[PortalWebInvoiceMainProc]
+	-- Add the parameters for the stored procedure here
+	@UserName varchar(25)
+AS
+BEGIN
+	SET NOCOUNT ON
+	DECLARE @RepCode varchar(4);
+	DECLARE @AccountType char(3);
+SELECT @RepCode = RepCode FROM Web_ActiveUsers where UserName=@UserName
+SELECT @AccountType = AccountType FROM Web_ActiveUsers where UserName=@UserName   
+SELECT     
+[InvNo]
+      ,[Typ]
+      ,[AcctNo]
+      ,[Date]
+      ,[Cmt]
+      ,[Coop]
+      ,[InvR]
+      ,[Desc]
+      ,[Qty]
+      ,[Pri]
+      ,[Tot]
+  FROM [dbo].[PortalWebInvoicesMain]
+                  
+where ((@AccountType = 'REP' and (InvR = @RepCode or AcctR = @RepCode)) or (@AccountType = 'OFF') )
+ORDER BY InvNo
+END
