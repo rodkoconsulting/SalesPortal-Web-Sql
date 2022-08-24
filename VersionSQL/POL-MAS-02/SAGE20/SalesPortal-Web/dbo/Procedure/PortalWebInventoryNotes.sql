faeebd69787d6851f096e2879268fd86c218e896
arodko <31354392+arodko@users.noreply.github.com>
2022-08-24 12:05:43 -0400

    Intial Commit PortalWebInventoryNotes Stored Procedure

---
+++ VersionSQL/POL-MAS-02/SAGE20/SalesPortal-Web/dbo/Procedure/PortalWebInventoryNotes.sql
@@ -0,0 +1,25 @@
+﻿/****** Object:  Procedure [dbo].[PortalWebInventoryNotes]    Committed by VersionSQL https://www.versionsql.com ******/
+
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[PortalWebInventoryNotes] 
+	-- Add the parameters for the stored procedure here
+	@ItemCode varchar(15)
+AS
+BEGIN
+	SELECT
+	UDF_NOTES_TASTING as NotesTast, 
+    UDF_NOTES_VINEYARD as NotesVine, 
+    UDF_NOTES_VINIFICATION as NotesVinif, 
+    UDF_NOTES_VARIETAL as NotesVar,
+    UDF_NOTES_PRODUCTION as NotesProd,
+    UDF_NOTES_AGING as NotesAge,
+    UDF_NOTES_ALTITUDE as NotesSoil,
+    UDF_NOTES_ORIENTATION as NotesOrien,
+    UDF_NOTES_SOIL as NotesViti
+	FROM [MAS_POL].dbo.CI_Item
+	WHERE ItemCode = @ItemCode
+END
