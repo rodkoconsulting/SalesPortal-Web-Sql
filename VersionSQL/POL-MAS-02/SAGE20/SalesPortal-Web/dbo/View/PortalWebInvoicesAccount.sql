e711f76fae2e25618fe5a4c7cdad5e8c60ab0e2d
arodko <31354392+arodko@users.noreply.github.com>
2022-08-24 18:22:29 -0400

    Initial commit PortalWebInvoicesAccount View

---
+++ VersionSQL/POL-MAS-02/SAGE20/SalesPortal-Web/dbo/View/PortalWebInvoicesAccount.sql
@@ -0,0 +1,25 @@
+﻿/****** Object:  View [dbo].[PortalWebInvoicesAccount]    Committed by VersionSQL https://www.versionsql.com ******/
+
+CREATE VIEW [dbo].[PortalWebInvoicesAccount] AS
+WITH InvHist AS
+(
+SELECT       DISTINCT ARDivisionNo, CustomerNo, SalespersonNo
+FROM            MAS_POL.dbo.AR_InvoiceHistoryHeader h
+WHERE ModuleCode = 'S/O' and InvoiceDate >= DATEADD(YEAR, - 2, GETDATE())
+UNION ALL
+SELECT        DISTINCT ARDivisionNo, CustomerNo, SalespersonNo
+FROM            MAS_POL.dbo.SO_InvoiceHeader h
+)
+SELECT DISTINCT
+	h.ARDivisionNo + h.CustomerNo AS AcctNo
+	, c.CustomerName AS 'Acct'
+	, c.UDF_AFFILIATIONS AS 'Aff'
+	, h.SalespersonNo AS InvR
+	, c.SalespersonNo AS AcctR
+	, CASE WHEN SUBSTRING(c.CUSTOMERTYPE, 3, 2) = 'ON' THEN 'On' ELSE 'Off' END AS Prem
+	, CASE WHEN s.UDF_TERRITORY = 'NY Metro' THEN 'NYM' WHEN UDF_TERRITORY = 'NY Long Island' THEN 'NYLI' WHEN UDF_TERRITORY = 'NY Upstate' THEN 'NYU' WHEN UDF_TERRITORY = 'NY Westchester / Hudson' THEN
+                          'NYW' WHEN UDF_TERRITORY = 'NJ' THEN 'NJ' WHEN UDF_TERRITORY = 'Pennsylvania' THEN 'PA' ELSE 'Manager' END AS Ter
+FROM InvHist h INNER JOIN
+	MAS_POL.dbo.AR_Customer AS c ON h.ARDivisionNo = c.ARDivisionNo AND h.CustomerNo = c.CustomerNo INNER JOIN
+    MAS_POL.dbo.SO_ShipToAddress AS a ON c.ARDivisionNo = a.ARDivisionNo AND c.CustomerNo = a.CustomerNo AND c.PrimaryShipToCode = a.ShipToCode INNER JOIN
+    MAS_POL.dbo.AR_UDT_SHIPPING AS s ON a.UDF_REGION_CODE = s.UDF_REGION_CODE
