e56de79bd69feb7cdd07de5a3c299970c7d03ccb
arodko <31354392+arodko@users.noreply.github.com>
2022-09-01 14:05:17 -0400

    Initial Commit

---
+++ VersionSQL/POL-MAS-02/SAGE20/SalesPortal-Web/dbo/Procedure/PortalWebOrderAccountProc.sql
@@ -0,0 +1,21 @@
+﻿/****** Object:  Procedure [dbo].[PortalWebOrderAccountProc]    Committed by VersionSQL https://www.versionsql.com ******/
+
+CREATE PROCEDURE [dbo].[PortalWebOrderAccountProc]
+	-- Add the parameters for the stored procedure here
+	@UserName varchar(25)
+AS
+BEGIN
+	SET NOCOUNT ON
+	DECLARE @RepCode varchar(4);
+	DECLARE @AccountType char(3);
+SELECT @RepCode = RepCode FROM Web_ActiveUsers where UserName=@UserName
+SELECT @AccountType = AccountType FROM Web_ActiveUsers where UserName=@UserName   
+SELECT [AcctNo]
+      ,[Acct]
+      ,[Aff]
+      ,[Prem]
+  FROM [dbo].[PortalWebOrdersAccount]
+                  
+where ((@AccountType = 'REP' and (Rep = @RepCode)) or (@AccountType = 'OFF') )
+ORDER BY AcctNo
+END
