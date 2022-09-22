d7727d1f71c218dc69f5f813b3e8cd4633502775
arodko <31354392+arodko@users.noreply.github.com>
2022-09-22 11:18:40 -0400

    Change Territory to pull from SalesPerson instead of account

--- VersionSQL/POL-MAS-02/SAGE20/SalesPortal-Web/dbo/View/PortalWebInvoicesAccount.sql
+++ VersionSQL/POL-MAS-02/SAGE20/SalesPortal-Web/dbo/View/PortalWebInvoicesAccount.sql
@@ -14,11 +14,12 @@ SELECT DISTINCT
 	h.ARDivisionNo + h.CustomerNo AS AcctNo
 	, c.CustomerName AS 'Acct'
 	, c.UDF_AFFILIATIONS AS 'Aff'
-	, h.SalespersonNo AS InvR
 	, c.SalespersonNo AS AcctR
+	, h.SalespersonNo as InvR
 	, CASE WHEN SUBSTRING(c.CUSTOMERTYPE, 3, 2) = 'ON' THEN 'On' ELSE 'Off' END AS Prem
-	, s.UDF_TERRITORY AS Ter
+	, CASE WHEN rep.UDF_TERRITORY<>'' THEN rep.UDF_TERRITORY ELSE 'NDD' END AS Ter
 FROM InvHist h INNER JOIN
 	MAS_POL.dbo.AR_Customer AS c ON h.ARDivisionNo = c.ARDivisionNo AND h.CustomerNo = c.CustomerNo INNER JOIN
+	MAS_POL.dbo.AR_Salesperson as rep ON c.SalespersonDivisionNo = rep.SalespersonDivisionNo and c.SalespersonNo = rep.SalespersonNo INNER JOIN
     MAS_POL.dbo.SO_ShipToAddress AS a ON c.ARDivisionNo = a.ARDivisionNo AND c.CustomerNo = a.CustomerNo AND c.PrimaryShipToCode = a.ShipToCode INNER JOIN
     MAS_POL.dbo.AR_UDT_SHIPPING AS s ON a.UDF_REGION_CODE = s.UDF_REGION_CODE
