6248c25633ae3899aa348e6034f2b0f4828ba381
polaneradmin <33095553+polaneradmin@users.noreply.github.com>
2023-10-24 17:25:04 -0400

    Add new procedure for AWS API users endpoint

---
+++ VersionSQL/POL-MAS-02/SAGE20/SalesPortal-Web/dbo/Procedure/PortalUsersProc.sql
@@ -0,0 +1,12 @@
+﻿/****** Object:  Procedure [dbo].[PortalUsersProc]    Committed by VersionSQL https://www.versionsql.com ******/
+
+CREATE PROCEDURE [dbo].[PortalUsersProc]
+	-- Add the parameters for the stored procedure here
+	@UserName varchar(25)
+AS
+BEGIN
+	SET NOCOUNT ON   
+SELECT Right([state],1) as [state]
+FROM Web_ActiveUsers where UserName=@UserName
+FOR JSON PATH, WITHOUT_ARRAY_WRAPPER
+END
