1d5935b8d71d07b60a027a31e8a26fedc31abe03
polaneradmin <33095553+polaneradmin@users.noreply.github.com>
2024-02-07 12:57:39 -0500

    Add active wine vendor contacts

---
+++ VersionSQL/POL-MAS-02/SAGE20/POL/dbo/View/AP_Vendor_Wine_Contacts_Active.sql
@@ -0,0 +1,9 @@
+﻿/****** Object:  View [dbo].[AP_Vendor_Wine_Contacts_Active]    Committed by VersionSQL https://www.versionsql.com ******/
+
+CREATE VIEW dbo.AP_Vendor_Wine_Contacts_Active
+AS
+SELECT DISTINCT v.VendorNo, v.VendorName, c.ContactName, c.EmailAddress, v.Sort
+FROM            MAS_POL.dbo.AP_Vendor AS v INNER JOIN
+                         MAS_POL.dbo.CI_Item AS i ON v.APDivisionNo = i.PrimaryAPDivisionNo AND v.VendorNo = i.PrimaryVendorNo INNER JOIN
+                         MAS_POL.dbo.AP_VendorContact AS c ON v.APDivisionNo = c.APDivisionNo AND v.VendorNo = c.VendorNo
+WHERE        (v.UDF_VEND_INACTIVE <> 'Y') AND (v.APDivisionNo = '00')
