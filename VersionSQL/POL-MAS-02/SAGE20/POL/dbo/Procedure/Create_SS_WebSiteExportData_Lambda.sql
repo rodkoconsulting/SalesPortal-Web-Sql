6747136e596ef11e83c15dd04de2ddb88b48c928
polaneradmin <33095553+polaneradmin@users.noreply.github.com>
2024-04-19 15:33:54 -0400

    Update objects for website synchronization process via AWS Lambda

---
+++ VersionSQL/POL-MAS-02/SAGE20/POL/dbo/Procedure/Create_SS_WebSiteExportData_Lambda.sql
@@ -0,0 +1,10 @@
+﻿/****** Object:  Procedure [dbo].[Create_SS_WebSiteExportData_Lambda]    Committed by VersionSQL https://www.versionsql.com ******/
+
+CREATE PROCEDURE dbo.Create_SS_WebSiteExportData_Lambda
+AS
+BEGIN
+	SET NOCOUNT ON;
+DELETE FROM WebSiteExportData_Previous_Lambda
+INSERT INTO WebSiteExportData_Previous_Lambda
+SELECT * from WebsiteExportData_Current
+END
---
+++ VersionSQL/POL-MAS-02/SAGE20/POL/dbo/Procedure/WebsiteExportDataDiff_Lambda.sql
@@ -0,0 +1,37 @@
+﻿/****** Object:  Procedure [dbo].[WebsiteExportDataDiff_Lambda]    Committed by VersionSQL https://www.versionsql.com ******/
+
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE dbo.WebsiteExportDataDiff_Lambda 
+	-- Add the parameters for the stored procedure here
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	SELECT
+			[Operation],
+			[Item Code], [Vendor],
+			[Description],
+			[Vintage], [Bottle Sizes],
+			[Country], [Region],
+			[Appellation], [Wine Type],
+			[Varietals], [Varietal Notes],
+			[Organic], [Biodynamic],
+			[Ratings - Wine Advocate], [Ratings - Wine Spectator], [Ratings - IWC], [Ratings - Burghound], [Ratings - Antonio Galloni], [Ratings - Other],
+                        [Review - Wine Advocate], [Review - Wine Spectator], [Review - IWC], [Review - Burghound], [Review - Antonio Galloni], [Review - Other],
+                        [Notes - Vineyard], [Notes - Orientation], [Notes - Soil], [Notes - Viticulture], [Notes - Vinification],
+                        [Notes - Aging Process], [Notes - Production], [Notes - Tasting],
+                        [ImageFile],
+                        [Natural],
+                        [Regen],
+                        [Vegan],
+                        [HauteValeur]
+			
+			FROM [dbo].[WebSiteExportData_Diff_Lambda] ORDER BY LastUpdated ASC, [Item Code] ASC, Operation ASC
+END
