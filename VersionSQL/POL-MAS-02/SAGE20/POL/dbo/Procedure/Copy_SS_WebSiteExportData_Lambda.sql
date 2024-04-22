d68f4c26d6713a34ce1b0a7e96c3b77d0be3aee2
polaneradmin <33095553+polaneradmin@users.noreply.github.com>
2024-04-22 14:52:00 -0400

    Add temporary procedure for initialing website synchronization process over AWS Lambda

---
+++ VersionSQL/POL-MAS-02/SAGE20/POL/dbo/Procedure/Copy_SS_WebSiteExportData_Lambda.sql
@@ -0,0 +1,10 @@
+﻿/****** Object:  Procedure [dbo].[Copy_SS_WebSiteExportData_Lambda]    Committed by VersionSQL https://www.versionsql.com ******/
+
+CREATE PROCEDURE [dbo].[Copy_SS_WebSiteExportData_Lambda]
+AS
+BEGIN
+	SET NOCOUNT ON;
+DELETE FROM WebSiteExportData_Previous_Lambda
+INSERT INTO WebSiteExportData_Previous_Lambda
+SELECT * from WebSiteExportData_Previous_Aws
+END
