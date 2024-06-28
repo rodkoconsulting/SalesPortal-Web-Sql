a77229610c6c4a4b449c66673cb1b6e0c1ff86cd
polaneradmin <33095553+polaneradmin@users.noreply.github.com>
2024-06-28 13:11:31 -0400

    Add procedures for modified Empire Wines AWS State Machine

---
+++ VersionSQL/POL-MAS-02/SAGE20/POL/dbo/Procedure/EmpireWines_CreateSnapshot.sql
@@ -0,0 +1,45 @@
+﻿/****** Object:  Procedure [dbo].[EmpireWines_CreateSnapshot]    Committed by VersionSQL https://www.versionsql.com ******/
+
+CREATE PROCEDURE [dbo].[EmpireWines_CreateSnapshot]
+	-- Add the parameters for the stored procedure here
+AS
+BEGIN
+	SET NOCOUNT ON;
+	DECLARE @ValidDate Datetime;
+	DECLARE @NextMonthDate Datetime;
+	DECLARE @NextMonthMonth int;
+	DECLARE @NextMonthYear int;
+	SET @NextMonthDate = DATEADD(month, 1, GETDATE());
+	SET @NextMonthMonth=MONTH(@NextMonthDate);
+	SET @NextMonthYear=Year(@NextMonthDate);
+	SET @ValidDate = CAST(CAST(@NextMonthMonth AS varchar(5))+'/1/'+CAST(@NextMonthYear As varchar(5)) as Datetime);
+TRUNCATE TABLE [dbo].[EmpireWines_Snapshot]
+INSERT INTO [dbo].[EmpireWines_Snapshot]
+	SELECT [SKU]
+      ,[Status]
+      ,[Importer/Supplier]
+      ,[Product Type]
+      ,[Product Subtype]
+      ,[Product Style]
+      ,[Producer Name]
+      ,[Product Name]
+      ,[Vintage]
+      ,[Grapes & Raw Materials]
+      ,[Country]
+      --,Replace(Replace([Region],char(10),''),char(13),'') as [Region]
+	  ,[Region]
+      ,[Appellation]
+      --,replace(replace(replace(Replace(Replace([Description],char(13),' '),char(10),' '),' ','<>'),'><',''),'<>',' ') as [Description]
+	  ,[Description]
+      ,[Features]
+      ,[Alcohol by volume]
+      ,[Size]
+      ,[Size Unit]
+      ,[Container Type]
+      ,[Case size]
+      ,[Pricing]
+      ,[ImageURL]
+      ,[TTB]
+   FROM [dbo].[EmpireWines_Query] dupe
+ where ValidDate in (SELECT MAX(VALIDDATE) FROM [dbo].[EmpireWines_Query] WHERE SKU=dupe.SKU AND VALIDDATE<=@ValidDate)
+END
---
+++ VersionSQL/POL-MAS-02/SAGE20/POL/dbo/Procedure/EmpireWines_QuerySnapshot.sql
@@ -0,0 +1,32 @@
+﻿/****** Object:  Procedure [dbo].[EmpireWines_QuerySnapshot]    Committed by VersionSQL https://www.versionsql.com ******/
+
+CREATE PROCEDURE [dbo].[EmpireWines_QuerySnapshot]
+	-- Add the parameters for the stored procedure here
+AS
+BEGIN
+	SET NOCOUNT ON;
+	SELECT [SKU]
+      ,[Status]
+      ,[Importer/Supplier]
+      ,[Product Type]
+      ,[Product Subtype]
+      ,[Product Style]
+      ,[Producer Name]
+      ,[Product Name]
+      ,[Vintage]
+      ,[Grapes & Raw Materials]
+      ,[Country]
+	  ,[Region]
+      ,[Appellation]
+	  ,[Description]
+      ,[Features]
+      ,[Alcohol by volume]
+      ,[Size]
+      ,[Size Unit]
+      ,[Container Type]
+      ,[Case size]
+      ,[Pricing]
+      ,[ImageURL]
+      ,[TTB]
+   FROM [dbo].[EmpireWines_Snapshot]
+END
