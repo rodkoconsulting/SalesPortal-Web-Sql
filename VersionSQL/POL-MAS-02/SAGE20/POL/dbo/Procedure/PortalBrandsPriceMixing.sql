65ee071bd6c7bc352c68fa1c96a415b01fed9d38
polaneradmin <33095553+polaneradmin@users.noreply.github.com>
2024-10-04 17:11:43 -0400

    Add query for new API endpoint for NJ price mixing

---
+++ VersionSQL/POL-MAS-02/SAGE20/POL/dbo/Procedure/PortalBrandsPriceMixing.sql
@@ -0,0 +1,9 @@
+﻿/****** Object:  Procedure [dbo].[PortalBrandsPriceMixing]    Committed by VersionSQL https://www.versionsql.com ******/
+
+CREATE PROCEDURE [dbo].[PortalBrandsPriceMixing]
+AS
+BEGIN
+		SELECT UDF_BRAND_NAME as [Brand]
+		FROM [MAS_POL].[dbo].[CI_UDT_BRANDS]
+		WHERE UDF_BRAND_MIXING = 'Y'
+END
