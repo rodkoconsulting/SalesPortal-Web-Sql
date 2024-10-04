5fd76391028df045688677f0589747a7fecc4b28
polaneradmin <33095553+polaneradmin@users.noreply.github.com>
2024-10-04 18:51:47 -0400

    Remove BrandPriceMixing procedure as refactored and moved to SalesPortal-Web database

--- VersionSQL/POL-MAS-02/SAGE20/POL/dbo/Procedure/PortalBrandsPriceMixing.sql
+++
@@ -1,9 +0,0 @@
-﻿/****** Object:  Procedure [dbo].[PortalBrandsPriceMixing]    Committed by VersionSQL https://www.versionsql.com ******/
-
-CREATE PROCEDURE [dbo].[PortalBrandsPriceMixing]
-AS
-BEGIN
-		SELECT UDF_BRAND_NAME as [Brand]
-		FROM [MAS_POL].[dbo].[CI_UDT_BRANDS]
-		WHERE UDF_BRAND_MIXING = 'Y'
-END
