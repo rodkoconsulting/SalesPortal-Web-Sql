b59b480463d8b9a28b22283d36a24f7a51e04872
polaneradmin <33095553+polaneradmin@users.noreply.github.com>
2024-10-04 19:19:45 -0400

    Remove deprecated QuickSight table

--- VersionSQL/POL-MAS-02/SAGE20/SalesPortal-Web/dbo/Table/UsersQuickSight.sql
+++
@@ -1,8 +0,0 @@
-﻿/****** Object:  Table [dbo].[UsersQuickSight]    Committed by VersionSQL https://www.versionsql.com ******/
-
-SET ANSI_NULLS ON
-SET QUOTED_IDENTIFIER ON
-CREATE TABLE [dbo].[UsersQuickSight](
-	[UserName] [nchar](10) NOT NULL,
-	[SalespersonNo] [varchar](4) NULL
-) ON [PRIMARY]
